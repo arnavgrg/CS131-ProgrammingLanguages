db120bec76ad9985745ff0132dd6891ccf5b14a4
Arnav Garg <arnavgrg@gmail.com>
2019-04-10 15:44:44 -0700

    Update hw1test.ml

--- Homework1/hw1test.ml
+++ Homework1/hw1test.ml
@@ -15,9 +15,15 @@ let my_set_union_test1 = equal_sets (set_union ["abc"; "def"] ["def"; "ghi"]) ["
 let my_set_union_test2 = equal_sets (set_union [1;1] [1;1;1;1]) [1]
 
 (*set intersection*)
+let my_set_intersection_test0 = equal_set [] (set_intersection [1;3;5] [2;4])
+let my_set_intersection_test1 = equal_sets (set_intersection [1;3;5;8] [5;9;8;9]) [5;8]
+let my_set_intersection_test2 = equal_sets (set_intersection [1;1;1] [1]) [1]
 
 (*set difference*)
+let my_set_diff_test0 = equal_sets (set_diff [9;8;7;6;5;4;3;2;1] [9;7;5;3;1]) [8;6;4;2]
+let my_set_diff_test1 = equal_sets (set_diff [1;1;1;1;1] [1;2;3;4;5]) []
 
 (*computed fixed point*)
+
 
 (*filter reachable*)
