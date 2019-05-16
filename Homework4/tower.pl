952de7479cd2db1f5ca9b30bba04d5d45a48c5fa
Arnav Garg <arnavgrg@gmail.com>
2019-05-15 19:39:40 -0700

    updated hw4

--- Homework4/tower.pl
+++ Homework4/tower.pl
@@ -1,3 +1,8 @@
+/*In general, lisp functions can be */
+
+% gprolog --consult-file tower.pl
+% @
+
 % -----
 % Function to create the transpose of a matrix
 % https://stackoverflow.com/questions/4280986/how-to-transpose-a-matrix-in-prolog
@@ -17,11 +22,6 @@ lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
 
 % -----
 % Helper methods
-% Rule definition to check if N is nonnegative
-% http://www.cse.unsw.edu.au/~billw/dictionaries/prolog/comparison.html
-check_nonnegative(N) :-
-    N >= 0.
-
 /* Rule that takes in a number N and a List L and returns true if the
 number of elements in the list equals N */
 check_length(N,L) :-
@@ -32,6 +32,18 @@ elements in the list fall within the range 1..N inclusive */
 % fd_domain(Vars, Lower, Upper) constraints each element X of Vars to take a value in Lower..Upper.
 check_domain(N,L) :-
     fd_domain(L,1,N).
+
+/*Helps match the C constraints left and top with the corresponding rows/columns */
+check([],[]).
+/*Takes Constraint List, Matrix in this format*/
+check([Head1|Tail1][Head2|Tail2]) :-
+    . 
+
+/*Helps match the C constraints right and bottom with the corresponding rows/columns */
+reverse_check([],[]).
+reverse_check([Head1|Tail1][]) :-
+    . 
+
 % -----
 
 % -----
@@ -41,7 +53,7 @@ http://www.gprolog.org/manual/html_node/ */
 % Rule definition for tower
 tower(N, T, counts(Top,Bottom,Left,Right)) :-
     % Ensure N is nonnegative
-    check_nonnegative(N),
+    N >= 0,
     /* http://www.swi-prolog.org/pldoc/man?predicate=length/2
     The predicate is non-deterministic, producing lists of increasing 
     length if List is a partial list and Int is unbound. */
@@ -50,22 +62,40 @@ tower(N, T, counts(Top,Bottom,Left,Right)) :-
     % Ensure each of the rows Top Bottom Left and Right have N elements
     % Maplist: https://riptutorial.com/prolog/example/8003/maplist--2-3-
     maplist(check_length(N),[Top,Bottom,Left,Right]),
+    maplist(check_length(N), T),
+    % Next we want to ensure all of them are within the range 1..N
+    maplist(check_domain(N), T),
     % https://stackoverflow.com/questions/6682987/prolog-map-procedure-that-applies-predicate-to-list-elements
     % Check if all elements in each row of T are unique
     maplist(fd_all_different, T),
-    % Next we want to ensure all of them are within the range 1..N
-    maplist(check_domain(N), T),
     % Transpose the matrix
     % We're guaranteed that each row and column in this matrix has different values in each row
-    transpose(T, T_transpose)
-    %
+    % Take the transponse and use the fd_all_different predicate to ensure columns also have unique values
+    transpose(T, T_transpose),
+    maplist(fd_all_different, T_transpose),
+    /*In order to have GNU Prolog report specific instances that satisfy the criteria, we need to use 
+    'fd_labeling(list of variables)' predicate as follows*/
+    /*Creates a list of lists T where each list has the constraints, that is, numbers between 1..N*/
+    % This predicate is re-executable on backtracking.
+    maplist(fd_labeling, T),
+    % Need to check if the edge/tower heights condition in counts matches each of columns
+    check(Top, T_transpose)
+    reverse_check(Bottom, T_tranpose)
+    % Need to check if the edge/tower heights condition in counts matches each of rows
+    check(Left, T) 
+    reverse_check(Right, T)
+    /* Finally, generate Top, Bottom, Left and Right using the constraints defined incase they haven't
+    already been defined */
+    maplist(fd_labeling,[Top,Bottom,Left,Right]).
 % -----
 
 % -----
 % Rule definition for plain tower
 plain_tower(N, T, counts(Top,Bottom,Left,Right)) :-
-    check_nonnegative(N),
-    1=1.
+    N >= 0,
+    check_length(N,T),
+    maplist(check_length(N),[Top,Bottom,Left,Right]),
+    maplist(check_length(N), T).
 % -----
 
 % -----
