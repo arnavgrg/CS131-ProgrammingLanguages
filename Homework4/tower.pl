d0722b96e54765e74eb6d3c343109f37e9d3ab0d
Arnav Garg <arnavgrg@gmail.com>
2019-05-16 15:25:49 -0700

    cleaned comments

--- Homework4/tower.pl
+++ Homework4/tower.pl
@@ -1,7 +1,4 @@
-/*In general, lisp functions can be */
-
-% gprolog --consult-file tower.pl
-% @
+/* GNU Prolog finite domain solver: http://www.gprolog.org/manual/html_node/ */
 
 % -----
 % Function to create the transpose of a matrix
@@ -24,16 +21,13 @@ lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
 % Helper methods
 /* Rule that takes in a number N and a List L and returns true if the
 number of elements in the list equals N */
-check_length(N,L) :-
-    length(L,N).
+check_length(N,L) :- length(L,N).
     
 /* Rule that takes in a number N and a List L and returns true if all
 elements in the list fall within the range 1..N inclusive */
-% fd_domain(Vars, Lower, Upper) constraints each element X of Vars to take a value in Lower..Upper.
-check_domain(N,L) :-
-    fd_domain(L,1,N).
+check_domain(N,L) :- fd_domain(L,1,N).
 
-/* */
+/*Function that verifies that the edge count = row count from that side*/
 verify_row(0,_,[]).
 verify_row(Counter,Maximum_value,[Head|Tail]) :- 
     Maximum_value >= Head,
@@ -62,36 +56,26 @@ reverse_check([Head_of_constraint_list|Tail_of_constraint_list],[Head_of_matrix|
 % -----
 
 % -----
-/* GNU Prolog finite domain solver
-http://www.gprolog.org/manual/html_node/ */
-
 % Rule definition for tower
 tower( N, T, counts(Top,Bottom,Left,Right)) :-
     % Ensure N is nonnegative
     N >= 0,
-    /* http://www.swi-prolog.org/pldoc/man?predicate=length/2
-    The predicate is non-deterministic, producing lists of increasing 
-    length if List is a partial list and Int is unbound. */
     % Ensure size of list of lists T == N.
     check_length(N,T),
     % Ensure each of the rows Top Bottom Left and Right have N elements
-    % Maplist: https://riptutorial.com/prolog/example/8003/maplist--2-3-
     maplist(check_length(N),[Top,Bottom,Left,Right]),
     maplist(check_length(N), T),
     % Next we want to ensure all of them are within the range 1..N
     maplist(check_domain(N), T),
-    % https://stackoverflow.com/questions/6682987/prolog-map-procedure-that-applies-predicate-to-list-elements
     % Check if all elements in each row of T are unique
     maplist(fd_all_different, T),
     % Transpose the matrix
-    % We're guaranteed that each row and column in this matrix has different values in each row
     % Take the transponse and use the fd_all_different predicate to ensure columns also have unique values
     transpose(T, T_transpose),
     maplist(fd_all_different, T_transpose),
     /*In order to have GNU Prolog report specific instances that satisfy the criteria, we need to use 
     'fd_labeling(list of variables)' predicate as follows*/
     /*Creates a list of lists T where each list has the constraints, that is, numbers between 1..N*/
-    % This predicate is re-executable on backtracking.
     maplist(fd_labeling, T),
     % Need to check if the edge/tower heights condition in counts matches each of columns
     check(Top, T_transpose),
@@ -110,17 +94,14 @@ plain_tower(N, T, counts(Top,Bottom,Left,Right)) :-
     N >= 0,
     check_length(N,T),
     maplist(check_length(N),[Top,Bottom,Left,Right]),
-    maplist(check_length(N), T).
+    maplist(check_length(N), T),
+    transpose(T, T_transpose).
 % -----
 
 % -----
 % Rule definition for ambiguous
 ambiguous(N, C, T1, T2) :-
     check_nonnegative(N),
-    % T1 and T2 get bound to different solutions 
-    /* This will always fail the first time as T1 and T2 will be the 
-    same. From here, it will backtrack and find another solution for 
-    T2 if there is a second solution to begin with. */
     tower(N,T1,C),
     tower(N,T2,C),
     T1 \== T2.
@@ -129,7 +110,6 @@ ambiguous(N, C, T1, T2) :-
 % -----
 % Performance based on CPU Time
 % http://gprolog.univ-paris1.fr/manual/html_node/gprolog048.html#statistics%2F2
-% https://stackoverflow.com/questions/34970061/display-the-execution-times-for-each-goal-of-a-predicate-clause
 
 %Function to run a test case and determine CPU time for the tower rule
 tower_test(Total_time) :-
@@ -146,10 +126,10 @@ plain_tower_test(Total_time) :-
     Total_time is Stop - Start. 
 
 %Function that actually computes the required ratio
-speedup(ratio_of_cpu_time) :-
+speedup(Ratio_of_cpu_time) :-
     % TT and PTT get bound to the respective CPU times returned.
     tower_test(TT),
     plain_tower_test(PTT),
     % Unifies the above arguments to the floating-point ratio
-    ratio_of_cpu_time is PTT/TT.   
+    Ratio_of_cpu_time is PTT/TT.   
 % -----
