023e7ab1847fc175fb9459d0e5a2fa0d78dab435
Arnav Garg <arnavgrg@gmail.com>
2019-06-02 05:16:34 -0700

    working IAMAT handler

--- Project/README.txt
+++ Project/README.txt
@@ -1,1 +1,2 @@
 https://tutorialedge.net/python/concurrency/asyncio-event-loops-tutorial/
+This helped me understand how to use asyncio event loops
--- Project/server.py
+++ Project/server.py
@@ -10,7 +10,7 @@ apiKey = "AIzaSyC7mqFJt6wYkoL3KGzAoBNXFGbWqi6ptDw"
 
 #Global file_name which will be changed within functions as needed
 file_name = None
-
+logfile = None
 #Global loop variable for asyncio
 loop = None
 
@@ -87,13 +87,94 @@ async def get_info(generated_url, limit):
                 write_to_file("ERROR: Async get request failed while trying WHATSAT!!!")
     return output_to_be_returned
 
+#Function to handle WHATSAT messages
+#Format:
+#WHATSAT kiwi.cs.ucla.edu 10 5
+async def handleWHATSAT(text):
+    if len(text) != 4:
+        return -1
+
+#Function to parse through the coordinates received by the client
+#Don't want to make this async because we want to wait till its completion (blocking)
+def parseCoords(coords):
+    latitude = None
+    longitude = None
+    signIndexes = []
+    dotIndexes = []
+    #Traverse through all the characters in the location string
+    for index,token in enumerate(coords, 1):
+        if token == '+' or token == '-':
+            #Push the index at which the + and - were found
+            signIndexes.append(index)
+        if token == '.':
+            dotIndexes.append(index)
+    #Want to sure there are only 2 of either + or - 
+    if len(signIndexes) != 2:
+        return None, None
+    #If there are more than two dots, then there is an issue
+    elif len(dotIndexes) != 2:
+        return None, None
+    else:
+        #Need to subtract 1 to get the signs, otherwise it gets the wrong indexes
+        latitude = coords[signIndexes[0]-1:signIndexes[1]-1]
+        longitude = coords[signIndexes[1]-1:]
+    return [latitude, longitude]
+
+#Function to handle IAMAT messages
+#Format: 
+#IAMAT kiwi.cs.ucla.edu +34.068930-118.445127 1520023934.918963997
+async def handleIAMAT(text):
+    #Should have exactly 4 elements in the list
+    if (len(text) != 4):
+        return -1
+    hostName = text[1] 
+    coords = text[2]
+    timeStamp = text[3]
+    #Validation for hostname and timestamp
+    if len(hostName) < 1:
+        return -1
+    elif len(timeStamp) < 1:
+        return -1
+    #Validate and parse coordinates
+    coordinates = parseCoords(coords)
+    if coordinates[0] == None or coordinates[1] == None:
+        return -1
+    #Otherwise everything is valid, so return success code 1
+    return 1
+
+#Parse through the message received to see how to handle it
+async def checkKeyword(text):
+    if text == None or len(text) == 0:
+        return -1
+    elif text[0] not in valid_commands:
+        return -1
+    elif text[0] == "IAMAT":
+        handleResult = await handleIAMAT(text)
+        return handleResult
+    elif text[0] == "WHATSAT":
+        handleResult = await handleWHATSAT(text)
+        return handleResult
+    #If neither, then it is an invalid command
+    #Servers should respond to invalid commands with a line that contains a question mark (?), a space, and then a copy of the invalid command.
+    else:
+        return -1
+
 #Callback function for start_server/create_server
 #Receives a (reader, writer) pair as two arguments, instances of the StreamReader and StreamWriter classes.
 #https://docs.python.org/3/library/asyncio-stream.html#asyncio.StreamReader
 #https://docs.python.org/3/library/asyncio-stream.html#asyncio.StreamWriter
 async def server_callback(reader, write):
-    await asyncio.sleep(1)
-    print("Reached!")
+    #read until EOF and return all read bytes -> (n=-1)
+    #Read as utf-8 bytestream, so need to convert it
+    read_data = await reader.read(n=-1)
+    #Record time the message was read at
+    #https://avilpage.com/2014/11/python-unix-timestamp-utc-and-their.html
+    receivedTime = time.time()
+    #Tokenize the message so it can be processed
+    message = " ".join(read_data.decode().split())
+    detectedKeyword = await checkKeyword(message.split())
+    if detectedKeyword == -1:
+        sys.exit(1)
 
 #Main driver function
 def main():
@@ -104,6 +185,7 @@ def main():
     file_name = str(sys.argv[1]) + '.log'
     #For some reason, if I don't do this, the file is never created
     open(file_name, 'a+').close()
+    global logfile
     logfile = open(file_name, 'a+')
     #https://docs.python.org/3/library/asyncio-eventloop.html#running-and-stopping-the-loop
     #https://docs.python.org/3/library/asyncio-eventloop.html#server-objects
@@ -120,7 +202,8 @@ def main():
     #A function that can be entered and exited multiple times, suspended and resumed each time, is called 
     #a coroutine.
     server = loop.run_until_complete(coro)
-    print("Connecting to server '{}'...".format(sys.argv[1]))
+    print("Connecting to server {}...".format(sys.argv[1]))
+    logfile.write("Opened connection to server..\n")
     #Run infinitely until keyboard interrupt
     try:
         loop.run_forever()
@@ -132,7 +215,7 @@ def main():
         loop.run_until_complete(server.wait_closed())
         #Close the loop, write last line to logfile and exit with status 0
         loop.close()
-        logfile.write("\n%s is shutting down..." % sys.argv[1])
+        logfile.write("%s is shutting down...\n" % sys.argv[1])
         logfile.close()
         sys.exit(0)
 
